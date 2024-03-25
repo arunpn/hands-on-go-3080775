c6063f5a3f42be078a23ba3d3909cba54e76099d
arunpn <oracle.arunp@gmail.com>
2024-03-25 06:52:12 +0000

    Lessons-Chapter1

--- packages/imports/begin/main.go
+++ packages/imports/begin/main.go
@@ -1,12 +1,19 @@
 // packages/imports/begin/main.go
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 // import the fmt package from the standard library
 
 // import the time package from the standard library
 
 func main() {
 	// use the fmt package to print the string "Hello Gopher!"
-
+	fmt.Println("Hello Gopher!")
 	// use the time package to print the current weekday
+
+	fmt.Printf("Today is %s", time.Now().Weekday())
 }
